ab7ba07f098e9fa51a91cad0cfbc56236a60afb8
AP2Gvalls <166146437+AP2Gvalls@users.noreply.github.com>
2025-03-20 18:37:39 +0100

    Add files via upload
    base del joc, carpeta amb img (podem modificar en un futur pero per el moment s'utilitzen aquestes), codi html i codi js amb la base (enllaçats entre si i amb el canva ja creat)

---
+++ Space_invaders/index.js
@@ -0,0 +1,5 @@
+const canvas = document.querySelector('canvas');
+const c = canvas.getContext('2d'); //context del fons (2d)
+
+canvas.width = innerWidth; //amplada de la pestanya
+canvas.height = innerHeight; //alçada de la pestanya
