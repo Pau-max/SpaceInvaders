73007523dc41cb0a49fcb55a874c1570cee0a201
PAUCEPEDA\pauce <a24pauceppor@inspedralbes.cat>
2025-04-09 16:56:53 +0200

    Todo lo del invader, movimiento, que aparezcan aleatoriamente, i que les puedas disparar

--- Space_invaders/index.js
+++ Space_invaders/index.js
@@ -80,7 +80,7 @@ class Projectile{
         this.position = position;
         this.velocity = velocity;
 
-        this.radius = 3;
+        this.radius = 4;
     }
 
     draw() {
@@ -99,8 +99,108 @@ class Projectile{
     }
 
 }
+//Clase para el invader
+class Invader {
+    // constructor del Invader
+    constructor({position}) {
+        this.velocity = {
+            x: 0,
+            y: 0
+        }
 
-const projectiles = []
+
+        // contant del Invader
+        const image = new Image();
+        image.src = './img/invader.png';
+        image.onload = () => {
+            const scale = 1;
+            // tamany imatge
+            this.image = image;
+            this.width = image.width * scale;
+            this.height = image.height * scale;
+            this.position = {
+                x: position.x,
+                y: position.y
+            }
+        }
+
+
+    }
+
+    // dibujar imagne del Invader
+    draw() {
+        //vc.fillStyle = 'red';
+        // c.fillRect(this.position.x, this.position.y, this.width, this.height);
+            
+            c.drawImage(
+                this.image,
+                this.position.x,
+                this.position.y,
+                this.width,
+                this.height
+            );
+        
+    }
+
+    update({velocity}){
+        if (this.image){
+            this.draw();
+            this.position.x += velocity.x;
+            this.position.y += velocity.y;
+        }
+    }
+
+}
+// Classe para que el invader se mueva
+class Grid {
+    constructor(){
+        this.position = {
+            x: 0,
+            y: 0
+        }
+
+        this.velocity = {
+            x: 3,
+            y: 0
+        }
+
+        this.invaders = []
+
+        const columns = Math.floor(Math.random() * 10 + 5)
+        const rows = Math.floor(Math.random() * 5 + 2)
+
+        this.width = columns * 30
+
+        for(let x = 0; x < columns; x++){
+            for(let y = 0; y < rows; y++){
+                this.invaders.push(new Invader({position:{
+                    x: x * 30,
+                    y: y * 30
+                }}))
+            }
+        }
+        console.log(this.invaders)
+    }
+
+    update (){
+        this.position.x += this.velocity.x
+        this.position.y += this.velocity.y
+        //Para que vayan bajando
+        this.velocity.y = 0 
+
+        if (this.position.x + this.width >= canvas.width || this.position.x <= 0){
+            this.velocity.x = -this.velocity.x
+            //Para que vayan bajando
+            this.velocity.y = 30
+        }
+    }
+
+
+}
+
+
+const projectiles = [];
+const grids = [];
 
 // constant para el Player
 const player = new Player();
@@ -116,6 +216,10 @@ const keys = {
         pressed: false
     }
 }
+
+let frames = 0
+let randomInterval = Math.floor(Math.random() * 500 + 500)
+console.log(randomInterval)
 
 
 // Mr.Canvaに表示されているすべてに応じて
@@ -133,8 +237,45 @@ function animate() {
         }else{
             projectiles.update()
         }
-        
     });
+
+    grids.forEach((grid, gridIndex) => {
+        grid.update()
+        grid.invaders.forEach((invader, i) => {
+            invader.update({velocity: grid.velocity})
+            projectiles.forEach( (projectile, j) => {
+                if (projectile.position.y - projectile.radius <= invader.position.y + invader.height && projectile.position.x + projectile.radius >= invader.position.x && projectile.position.x - projectile.radius <= invader.position.x + invader.width && projectile.position.y + projectile.radius >= invader.position.y){
+
+                    setTimeout(() => {
+                        const invaderFound = grid.invaders.find(
+                            (invader2) => invader2 === invader
+                        )
+                        const projectileFound = projectiles.find(
+                            (projectile2) => projectile2 === projectile
+                        )
+
+                        //remove invader and projectile
+                        if (invaderFound && projectileFound ){
+                        grid.invaders.splice(i, 1)
+                        projectiles.splice(j, 1)
+
+                        if(grid.invaders.length > 0) {
+                            const firstInvader = grid.invaders [0]
+                            const lastInvader = grid.invaders [grid.invaders.length - 1]
+
+                            grid.width = lastInvader.position.x - firstInvader.position.x +lastInvader.width
+                            grid.position.x = firstInvader.position.x
+                        }else{
+                            grids.splice(gridIndex, 1)
+                        }
+                        }
+                    }, 0)
+
+                }
+            })
+
+        })
+    })
 
     if (keys.a.pressed && player.position.x >= 0){
         player.velocity.x = -7;
@@ -146,8 +287,18 @@ function animate() {
         player.velocity.x = 0;
         player.rotation = 0;
     }
-}
 
+    
+    //Spawnear Invaders
+    if(frames % randomInterval === 0){
+        grids.push(new Grid())
+        randomInterval = Math.floor(Math.random() * 500 + 500)
+        frames = 0
+        console.log(randomInterval)
+    }
+
+    frames++
+}
 
 // execute order animate 
 animate();
