ee1db79511dd448d1b3058562ba6eda6d85ca995
PAUCEPEDA\pauce <a24pauceppor@inspedralbes.cat>
2025-04-10 20:39:26 +0200

    Explosin de los Invader

--- Space_invaders/index.js
+++ Space_invaders/index.js
@@ -1,6 +1,5 @@
 const canvas = document.querySelector('canvas');
 const c = canvas.getContext('2d'); //context del fons (2d)
-
 canvas.width = innerWidth; //amplada de la pestanya
 canvas.height = innerHeight; //alçada de la pestanya
 
@@ -36,37 +35,37 @@ class Player {
     }
 
 
-    
+
 
 
     // dibujar imagne del la nave del player
     draw() {
         //vc.fillStyle = 'red';
         // c.fillRect(this.position.x, this.position.y, this.width, this.height);
-            c.save()
-            c.translate(
-                player.position.x + player.width / 2, 
-                player.position.y + player.height / 2
-            )
-            c.rotate(this.rotation);
-            c.translate(
-                -player.position.x - player.width / 2, 
-                -player.position.y - player.height / 2
-            )
-            c.drawImage(
-                this.image,
-                this.position.x,
-                this.position.y,
-                this.width,
-                this.height
-            );
+        c.save()
+        c.translate(
+            player.position.x + player.width / 2,
+            player.position.y + player.height / 2
+        )
+        c.rotate(this.rotation);
+        c.translate(
+            -player.position.x - player.width / 2,
+            -player.position.y - player.height / 2
+        )
+        c.drawImage(
+            this.image,
+            this.position.x,
+            this.position.y,
+            this.width,
+            this.height
+        );
 
-            c.restore()
-        
+        c.restore()
+
     }
 
-    update(){
-        if (this.image){
+    update() {
+        if (this.image) {
             this.draw();
             this.position.x += this.velocity.x;
         }
@@ -75,8 +74,8 @@ class Player {
 }
 
 //es per crear el projectil
-class Projectile{
-    constructor({position, velocity}){
+class Projectile {
+    constructor({ position, velocity }) {
         this.position = position;
         this.velocity = velocity;
 
@@ -92,17 +91,70 @@ class Projectile{
         c.closePath();
     }
 
-    update(){
+    update() {
         this.draw();
         this.position.x += this.velocity.x;
         this.position.y += this.velocity.y;
     }
-
 }
+//Particula para la explosion del invader
+class Particle {
+    constructor({ position, velocity, radius, color }) {
+        this.position = position;
+        this.velocity = velocity;
+
+        this.radius = radius;
+        this.color = color;
+        this.opacity = 1
+    }
+
+    draw() {
+        c.save()
+        c.globalAlpha = this.opacity
+        c.beginPath();
+        c.arc(this.position.x, this.position.y, this.radius, 0,
+            Math.PI * 2);
+        c.fillStyle = this.color;
+        c.fill();
+        c.closePath();
+        c.restore()
+    }
+
+    update() {
+        this.draw();
+        this.position.x += this.velocity.x;
+        this.position.y += this.velocity.y;
+
+        this.opacity -= 0.01
+    }
+}
+
+class InvaderProjectile {
+    constructor({ position, velocity }) {
+        this.position = position;
+        this.velocity = velocity;
+
+        this.width = 3
+        this.height = 10
+
+    }
+
+    draw() {
+        c.fillStyle = 'white'
+        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
+
+    update() {
+        this.draw();
+        this.position.x += this.velocity.x;
+        this.position.y += this.velocity.y;
+    }
+}
+
 //Clase para el invader
 class Invader {
     // constructor del Invader
-    constructor({position}) {
+    constructor({ position }) {
         this.velocity = {
             x: 0,
             y: 0
@@ -131,29 +183,43 @@ class Invader {
     draw() {
         //vc.fillStyle = 'red';
         // c.fillRect(this.position.x, this.position.y, this.width, this.height);
-            
-            c.drawImage(
-                this.image,
-                this.position.x,
-                this.position.y,
-                this.width,
-                this.height
-            );
-        
+
+        c.drawImage(
+            this.image,
+            this.position.x,
+            this.position.y,
+            this.width,
+            this.height
+        );
+
     }
 
-    update({velocity}){
-        if (this.image){
+    update({ velocity }) {
+        if (this.image) {
             this.draw();
             this.position.x += velocity.x;
             this.position.y += velocity.y;
         }
     }
 
+    shoot(invaderProjectiles) {
+        invaderProjectiles.push(new InvaderProjectile({
+            position: {
+                x: this.position.x + this.width / 2,
+                y: this.position.y + this.height
+            },
+            velocity: {
+                x: 0,
+                y: 5
+            }
+        }))
+
+    }
+
 }
 // Classe para que el invader se mueva
 class Grid {
-    constructor(){
+    constructor() {
         this.position = {
             x: 0,
             y: 0
@@ -171,24 +237,26 @@ class Grid {
 
         this.width = columns * 30
 
-        for(let x = 0; x < columns; x++){
-            for(let y = 0; y < rows; y++){
-                this.invaders.push(new Invader({position:{
-                    x: x * 30,
-                    y: y * 30
-                }}))
+        for (let x = 0; x < columns; x++) {
+            for (let y = 0; y < rows; y++) {
+                this.invaders.push(new Invader({
+                    position: {
+                        x: x * 30,
+                        y: y * 30
+                    }
+                }))
             }
         }
         console.log(this.invaders)
     }
 
-    update (){
+    update() {
         this.position.x += this.velocity.x
         this.position.y += this.velocity.y
         //Para que vayan bajando
-        this.velocity.y = 0 
+        this.velocity.y = 0
 
-        if (this.position.x + this.width >= canvas.width || this.position.x <= 0){
+        if (this.position.x + this.width >= canvas.width || this.position.x <= 0) {
             this.velocity.x = -this.velocity.x
             //Para que vayan bajando
             this.velocity.y = 30
@@ -201,25 +269,46 @@ class Grid {
 
 const projectiles = [];
 const grids = [];
+const invaderProjectiles = []
+const particles = []
+
 
 // constant para el Player
 const player = new Player();
 
 const keys = {
-    a:{
+    a: {
         pressed: false
     },
-    d:{
+    d: {
         pressed: false
     },
-    space:{
+    w: {
         pressed: false
     }
 }
 
 let frames = 0
 let randomInterval = Math.floor(Math.random() * 500 + 500)
-console.log(randomInterval)
+//Creacion de la particulas
+function createParticles({ object, color }) {
+    for (let i = 0; i < 15; i++) {
+        //Caracteristicas de las particulas 
+        particles.push(new Particle({
+
+            position: {
+                x: object.position.x + object.width / 2,
+                y: object.position.y + object.height / 2
+            },
+            velocity: {
+                x: (Math.random() - 0.5) * 2,
+                y: (Math.random() - 0.5) * 2
+            },
+            radius: Math.random() * 3,
+            color: color || '#BAA0DE'
+        }))
+    }
+}
 
 
 // Mr.Canvaに表示されているすべてに応じて
@@ -229,22 +318,65 @@ function animate() {
     c.fillStyle = 'black';
     c.fillRect(0, 0, canvas.width, canvas.height);
     player.update();
+    particles.forEach((particel, i) => {
+        if (particel.opacity <= 0) {
+            setTimeout(() => {
+                particles.splice(i, 1)
+            }, 0)
+
+        } else {
+            particel.update()
+        }
+    })
+
+    console.log(particles)
+
+    invaderProjectiles.forEach(invaderProjectile => {
+        if (invaderProjectile.position.y + invaderProjectile.height >= canvas.height) {
+            setTimeout(() => {
+                invaderProjectiles.splice(index, 1);
+            }, 0)
+        } else invaderProjectile.update()
+        
+        //projectil golpea al player
+        if (invaderProjectile.position.y + invaderProjectile.height >= player.position.y && invaderProjectile.position.x + invaderProjectile.width >= player.position.x && invaderProjectile.position.x <= player.position.x + player.width) {
+            setTimeout(() => {
+                invaderProjectiles.splice(index, 1);
+            }, 0)
+            console.log('you lose')
+            createParticles({
+                object: player,
+                color: 'white'
+            })
+        }
+    })
+
+
+
     projectiles.forEach((projectiles, index) => {
-        if(projectiles.position.y + projectiles.radius <= 0){
+        if (projectiles.position.y + projectiles.radius <= 0) {
             setTimeout(() => {
                 projectiles.splice(index, 1)
             }, 0)
-        }else{
+        } else {
             projectiles.update()
         }
     });
 
     grids.forEach((grid, gridIndex) => {
         grid.update()
+
+        //Spawnear Projectiles de los Invaders
+        if (frames % 100 === 0 && grid.invaders.length > 0) {
+            grid.invaders[Math.floor(Math.random() * grid.invaders.length)].shoot(invaderProjectiles)
+        }
+
         grid.invaders.forEach((invader, i) => {
-            invader.update({velocity: grid.velocity})
-            projectiles.forEach( (projectile, j) => {
-                if (projectile.position.y - projectile.radius <= invader.position.y + invader.height && projectile.position.x + projectile.radius >= invader.position.x && projectile.position.x - projectile.radius <= invader.position.x + invader.width && projectile.position.y + projectile.radius >= invader.position.y){
+            invader.update({ velocity: grid.velocity })
+            // los projectiles golpean a los invaders
+            projectiles.forEach((projectile, j) => {
+                if (projectile.position.y - projectile.radius <= invader.position.y + invader.height && projectile.position.x + projectile.radius >= invader.position.x && projectile.position.x - projectile.radius <= invader.position.x + invader.width && projectile.position.y + projectile.radius >= invader.position.y) {
+
 
                     setTimeout(() => {
                         const invaderFound = grid.invaders.find(
@@ -254,20 +386,24 @@ function animate() {
                             (projectile2) => projectile2 === projectile
                         )
 
-                        //remove invader and projectile
-                        if (invaderFound && projectileFound ){
-                        grid.invaders.splice(i, 1)
-                        projectiles.splice(j, 1)
+                        //eliminar projectiles y invaders
+                        if (invaderFound && projectileFound) {
+                            createParticles({
+                                object: invader,
+                            })
 
-                        if(grid.invaders.length > 0) {
-                            const firstInvader = grid.invaders [0]
-                            const lastInvader = grid.invaders [grid.invaders.length - 1]
+                            grid.invaders.splice(i, 1)
+                            projectiles.splice(j, 1)
 
-                            grid.width = lastInvader.position.x - firstInvader.position.x +lastInvader.width
-                            grid.position.x = firstInvader.position.x
-                        }else{
-                            grids.splice(gridIndex, 1)
-                        }
+                            if (grid.invaders.length > 0) {
+                                const firstInvader = grid.invaders[0]
+                                const lastInvader = grid.invaders[grid.invaders.length - 1]
+
+                                grid.width = lastInvader.position.x - firstInvader.position.x + lastInvader.width
+                                grid.position.x = firstInvader.position.x
+                            } else {
+                                grids.splice(gridIndex, 1)
+                            }
                         }
                     }, 0)
 
@@ -277,10 +413,10 @@ function animate() {
         })
     })
 
-    if (keys.a.pressed && player.position.x >= 0){
+    if (keys.a.pressed && player.position.x >= 0) {
         player.velocity.x = -7;
         player.rotation = -0.15;
-    } else if (keys.d.pressed && player.position.x + player.width <= canvas.width ){
+    } else if (keys.d.pressed && player.position.x + player.width <= canvas.width) {
         player.velocity.x = 7;
         player.rotation = 0.15;
     } else {
@@ -288,14 +424,15 @@ function animate() {
         player.rotation = 0;
     }
 
-    
+
     //Spawnear Invaders
-    if(frames % randomInterval === 0){
+    if (frames % randomInterval === 0) {
         grids.push(new Grid())
         randomInterval = Math.floor(Math.random() * 500 + 500)
         frames = 0
-        console.log(randomInterval)
     }
+
+
 
     frames++
 }
@@ -305,41 +442,41 @@ animate();
 
 //serveix per a quan presionis una tecla s'activi el mov
 addEventListener('keydown', ({ key }) => {
-    switch (key){
+    switch (key) {
         case 'a':
             console.log('a')
             keys.a.pressed = true;
             break;
-        
+
         case 'd':
             console.log('d')
             keys.d.pressed = true;
             break;
 
-        case ' ':
-            console.log('space')
-            projectiles.push (
+        case 'w':
+            console.log('w')
+            projectiles.push(
                 new Projectile({
-                position: {
-                    x: player.position.x + player.width / 2,
-                    y: player.position.y
-                },
-                velocity: {
-                    x: 0,
-                    y: -10
-                }
-            })
-        )
+                    position: {
+                        x: player.position.x + player.width / 2,
+                        y: player.position.y
+                    },
+                    velocity: {
+                        x: 0,
+                        y: -10
+                    }
+                })
+            )
             break;
     }
 })
 
 addEventListener('keyup', ({ key }) => {
-    switch (key){
+    switch (key) {
         case 'a':
             keys.a.pressed = false;
             break;
-        
+
         case 'd':
             keys.d.pressed = false;
             break;
